dfe7ec3b556a983ffcb07267d2cbc006dd52f03b
Krishna Patidar <113329860+krishna-p060@users.noreply.github.com>
2023-10-21 10:18:03 +0530

    js file added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.19",
+// };
+
 require("@nomicfoundation/hardhat-toolbox");
 
+
+require('dotenv').config();
+const {API_URL, PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  
+  etherscan: {
+    apiKey: "HIZW6B2NVJNCWE8UEVFNHBFN8JJ571RQ6V"
+  },
+  networks: {
+    fuji: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+ },
 };
+
